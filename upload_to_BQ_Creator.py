26c05f547a7dd787a8e8282b12cfd0984cdd68f0
Montereytony <montereytony@gmail.com>
2023-04-07 11:14:31 -0700

    Moved the database variable to the top of the file and added a delimiter variable so we do not have to look for it in the code since it is in two locations

--- upload_to_BQ_Creator.py
+++ upload_to_BQ_Creator.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-#
 #
 # Tony's Code to Automate uploading csv files
 #
@@ -7,12 +5,18 @@
 #
 
 #
+# Define the two needed variables, name of dateset and delimiter
+#
+bq_db_name = "external_data_db"
+delim_var = '!'
+
+#
 # Load library needed to read in all csv files
 #
 import glob
 
 #
-# Load CSV files in folder 
+# Load CSV files in folder
 #
 csv_files = []
 for file in glob.glob("*.csv"):
@@ -23,13 +27,12 @@ for file in glob.glob("*.csv"):
 #
 
 for csv_filename in csv_files:
-    bq_db_name = "external_data_db"
     bq_table_name = csv_filename.split(".")
-    load_cmd = 'bq load --replace=true --field_delimiter="|" --autodetect  --skip_leading_rows=2 --source_format=CSV '
+    load_cmd = "bq load --replace=true --field_delimiter=\'" + delim_var + "\' --skip_leading_rows=2 --source_format=CSV "
     load_str = load_cmd +  bq_db_name  + "." + bq_table_name[0] + " " + csv_filename
     file_variable = open(csv_filename)
     all_lines_variable = file_variable.readlines()
-    new_str = all_lines_variable[1].replace("|", ":STRING,") + ":STRING"
+    new_str = all_lines_variable[1].replace(delim_var,":STRING,") + ":STRING"
     new_str = ''.join(new_str.split('\n'))
     new_str = load_str +  "  " + new_str
     print("echo \'Doing  " + csv_filename + "\'" )
